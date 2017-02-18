b9bd4d46864f9ececc495bb00412014d04354cfd
jammcc <jammcc27@gmail.com>
2017-02-18 14:29:33 -0500

    rename Django name

--- encounter_site/urls.py
+++ encounter_site/urls.py
@@ -1,4 +1,4 @@
-"""django_bookmarks URL Configuration
+"""encounter_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
